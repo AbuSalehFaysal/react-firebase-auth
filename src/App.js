fcc26130f6a6a8fa4daf0a77be3692090d9ee778
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2021-03-17 18:08:18 +0600

    functionality added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase-config';
+import { useState } from 'react';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+
+  const handleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        // var credential = result.credential;
+
+        // // This gives you a Google Access Token. You can use it to access the Google API.
+        // var token = credential.accessToken;
+        // // The signed-in user info.
+        var user = result.user;
+        // console.log(user);
+        const {displayName, photoURL, email} = user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signedInUser);
+        console.log(displayName, photoURL, email);
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  }
+
+  const handleSignOut  = () => {
+    firebase.auth().signOut().then(res => {
+      // Sign-out successful.
+      const signedOutUser = {
+        isSignedIn: false,
+        name: '',
+        photo: '',
+        email: ''
+      }
+      setUser(signedOutUser);
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button> : <button onClick={handleSignIn}>Sign In</button>
+        
+      }
+      
+      {
+        user.isSignedIn && <p> Welcome, {user.name}</p>
+      }
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlUaSDeJRurqS2kyPSi_txw1kAzF7I5vs",
+    authDomain: "ema-john-simple-1010.firebaseapp.com",
+    projectId: "ema-john-simple-1010",
+    storageBucket: "ema-john-simple-1010.appspot.com",
+    messagingSenderId: "1070735274134",
+    appId: "1:1070735274134:web:fb9b2b39afbff89fb8d2b7"
+  };
+
+  export default firebaseConfig;
